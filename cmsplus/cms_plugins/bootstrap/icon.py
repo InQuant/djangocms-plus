d9a5726180a31a2f61cf9e8a5a62a31e9e15bc51
Lutz Mende <lutz.mende@inquant.de>
2025-04-19 16:17:51 +0200

    - refactored icon plugin

--- cmsplus/cms_plugins/bootstrap/icon.py
+++ cmsplus/cms_plugins/bootstrap/icon.py
@@ -5,8 +5,8 @@ from django import forms
 from django.contrib.staticfiles import finders
 from django.core.exceptions import ImproperlyConfigured
 from django.forms.renderers import get_default_renderer
-from django.utils.functional import cached_property
 from django.utils.safestring import mark_safe
+from django.utils.encoding import force_str
 from django.utils.translation import gettext_lazy as _
 
 from cmsplus.app_settings import cmsplus_settings as cps
@@ -14,7 +14,120 @@ from cmsplus.forms import LinkFormMixin
 from cmsplus.plugin_base import LinkPluginMixin
 from cmsplus.cms_plugins.bootstrap.base import BootstrapFormBase, BootstrapPluginBase
 
+BOOTSTRAP_ICONS = []
+FONT_AWESOME_ICONS = []
+FONTELLO_ICONS = []
 
+# Icon Finder functions
+# ---------------------
+def get_bootstrap_icons():
+    global BOOTSTRAP_ICONS
+
+    if BOOTSTRAP_ICONS:
+        return BOOTSTRAP_ICONS
+
+    icons = []
+    path = finders.find(cps.ICONS_BOOTSTRAP['meta'])
+    if not path or not os.path.exists(path):
+        raise ImproperlyConfigured('ICONS_BOOTSTRAP: meta path is not existing (%s)' % path)
+
+    with open(path, 'rb') as f:
+        raw_data = f.read()
+    try:
+        data = json.loads(raw_data)
+    except TypeError:
+        # Python 3.5 compatibility
+        data = json.loads(raw_data.decode('utf-8'))
+
+    for key, _ in data.items():
+        _key = force_str(key)
+        icons.append({
+            'name': _key,
+            'label': _key,
+            'font_class_name': f'bi bi-{_key}',
+        })
+    BOOTSTRAP_ICONS = icons
+    return icons
+
+def get_fontawesome_icons():
+    global FONT_AWESOME_ICONS
+
+    if FONT_AWESOME_ICONS:
+        return FONT_AWESOME_ICONS
+
+    icons = []
+    # list of dicts:
+    # { 'name': '',
+    #   'label': '',
+    #   'font_class_name': '', }
+
+    path = finders.find(cps.ICONS_FONTAWESOME['meta'])
+    if not path or not os.path.exists(path):
+        raise ImproperlyConfigured('ICONS_FONTAWESOME: meta path is not existing (%s)' % path)
+
+    with open(path, 'rb') as f:
+        raw_data = f.read()
+    try:
+        data = json.loads(raw_data)
+    except TypeError:
+        # Python 3.5 compatibility
+        data = json.loads(raw_data.decode('utf-8'))
+
+    for key, value in data.items():
+        # check styles ['brands', 'solid', 'regular']
+        for style in value.get('styles'):
+            if style == "solid":
+                font_class_name = "fas fa-%s" % key
+            elif style == "brands":
+                font_class_name = "fab fa-%s" % key
+            elif style == "regular":
+                font_class_name = "far fa-%s" % key
+            else:
+                raise ValueError("%s style not defined" % style)
+
+            icons.append({
+                'name': force_str(key),
+                'label': force_str(value.get('label')),
+                'font_class_name': force_str(font_class_name),
+            })
+    FONT_AWESOME_ICONS = icons
+    return icons
+
+def get_fontello(attrs):
+    global FONTELLO_ICONS
+
+    if FONTELLO_ICONS:
+        return FONTELLO_ICONS
+
+    icons = []
+    path = finders.find(attrs.get('meta'))
+    if not path or not os.path.exists(path):
+        raise ImproperlyConfigured('CMSPLUS SETTINGS - ICONS: path is not existing (%s)' % path)
+
+    with open(path, 'rb') as f:
+        raw_data = f.read()
+    try:
+        data = json.loads(raw_data)
+    except TypeError:
+        # Python 3.5 compatibility
+        data = json.loads(raw_data.decode('utf-8'))
+
+    prefix = data.get('css_prefix_text', 'icon-')
+    for glyph in data.get('glyphs', []):
+        if not glyph.get('css'):
+            continue
+
+        gl = force_str(glyph.get('css'))
+        icons.append({
+            'name': gl,
+            'label': gl,
+            'font_class_name': force_str("%s%s" % (prefix, gl)),
+        })
+    FONTELLO_ICONS = icons
+    return icons
+
+# IconField
+# ---------
 class IconFieldWidget(forms.Widget):
     template_name = "cmsplus/admin/widgets/icon.html"
     icons = []
@@ -22,135 +135,49 @@ class IconFieldWidget(forms.Widget):
     def __init__(self, attrs=None):
         super().__init__(attrs)
 
-        # add fontawesome
-        if cps.ICONS_FONTAWESOME and cps.ICONS_FONTAWESOME_SHOW:
-            self.icons += self.get_fontawesome_icons
 
         # add bootstrap
         if cps.ICONS_BOOTSTRAP and cps.ICONS_BOOTSTRAP_SHOW:
-            self.icons += self.get_bootstrap_icons
+            self.icons = get_bootstrap_icons()
+
+        # add fontawesome
+        if cps.ICONS_FONTAWESOME and cps.ICONS_FONTAWESOME_SHOW:
+            self.icons += get_fontawesome_icons()
 
         # add icons
         for font in getattr(cps, 'ICONS_FONTELLO', []):
-            self.icons += self.get_fontello(font)
+            self.icons += get_fontello(font)
 
     def render(self, name, value, add_to_class=None, attrs=None, renderer=None):
         if renderer is None:
             renderer = get_default_renderer()
 
-        icons = IconFieldWidget.icons
+        icons = self.icons
 
         # add "no-icon" if not required
         if not attrs.get('required'):
             if not any(f['font_class_name'] == 'cmsplus-icon-none' for f in icons):
                 icons.insert(0, {
-                    'name': _('No icon'),
-                    'label': _('No icon'),
+                    'name': 'No icon',
+                    'label': 'No icon',
                     'font_class_name': 'cmsplus-icon-none'
                 })
 
         context = self.get_context(name, value, attrs)
-        context['widget']['icons'] = IconFieldWidget.icons
+        context['widget']['icons_json'] = json.dumps(icons)
         context['widget']['value'] = value
         context['widget']['name'] = name
         context['widget']['attrs'] = attrs
         context['widget']['add_to_class'] = add_to_class
         return mark_safe(renderer.render(self.template_name, context))
 
-    @cached_property
-    def get_bootstrap_icons(self):
-        icons = []
-        path = finders.find(cps.ICONS_BOOTSTRAP['meta'])
-        if not path or not os.path.exists(path):
-            raise ImproperlyConfigured('ICONS_BOOTSTRAP: meta path is not existing (%s)' % path)
-
-        with open(path, 'rb') as f:
-            raw_data = f.read()
-        try:
-            data = json.loads(raw_data)
-        except TypeError:
-            # Python 3.5 compatibility
-            data = json.loads(raw_data.decode('utf-8'))
-
-        for key, value in data.items():
-            icons.append({
-                'name': key,
-                'label': key,
-                'font_class_name': f'bi bi-{key}',
-            })
-        return icons
-
-    @cached_property
-    def get_fontawesome_icons(self):
-        icons = []
-        # list of dicts:
-        # { 'name': '',
-        #   'label': '',
-        #   'font_class_name': '', }
-
-        path = finders.find(cps.ICONS_FONTAWESOME['meta'])
-        if not path or not os.path.exists(path):
-            raise ImproperlyConfigured('ICONS_FONTAWESOME: meta path is not existing (%s)' % path)
-
-        with open(path, 'rb') as f:
-            raw_data = f.read()
-        try:
-            data = json.loads(raw_data)
-        except TypeError:
-            # Python 3.5 compatibility
-            data = json.loads(raw_data.decode('utf-8'))
-
-        for key, value in data.items():
-            # check styles ['brands', 'solid', 'regular']
-            for style in value.get('styles'):
-                if style == "solid":
-                    font_class_name = "fas fa-%s" % key
-                elif style == "brands":
-                    font_class_name = "fab fa-%s" % key
-                elif style == "regular":
-                    font_class_name = "far fa-%s" % key
-                else:
-                    raise ValueError("%s style not defined" % style)
-
-                icons.append({
-                    'name': key,
-                    'label': value.get('label'),
-                    'font_class_name': font_class_name,
-                })
-        return icons
-
-    @staticmethod
-    def get_fontello(attrs):
-        icons = []
-        path = finders.find(attrs.get('meta'))
-        if not path or not os.path.exists(path):
-            raise ImproperlyConfigured('CMSPLUS SETTINGS - ICONS: path is not existing (%s)' % path)
-
-        with open(path, 'rb') as f:
-            raw_data = f.read()
-        try:
-            data = json.loads(raw_data)
-        except TypeError:
-            # Python 3.5 compatibility
-            data = json.loads(raw_data.decode('utf-8'))
-
-        prefix = data.get('css_prefix_text', 'icon-')
-        for glyph in data.get('glyphs', []):
-            if not glyph.get('css'):
-                continue
-
-            icons.append({
-                'name': glyph.get('css'),
-                'label': glyph.get('css'),
-                'font_class_name': "%s%s" % (prefix, glyph.get('css')),
-            })
-        return icons
-
 
 class IconField(forms.CharField):
     widget = IconFieldWidget
 
 
+# Icon
+# ----
 class IconFormMixin(forms.Form):
     icon = IconField(required=True)
 
--- cmsplus/static/cmsplus/admin/icon_plugin/js/icon_plugin.js
+++ cmsplus/static/cmsplus/admin/icon_plugin/js/icon_plugin.js
@@ -4,31 +4,48 @@ document.addEventListener("DOMContentLoaded", function () {
     const selectedIcon = widget.querySelector(".selected-icon");
     const selectedIconName = widget.querySelector(".selected-icon-name");
     const hiddenInput = widget.querySelector(".hidden-icon-field");
-    const iconButtons = widget.querySelectorAll(".icon-select-btn");
-    const iconSearch = widget.querySelector(".icon-search");
 
     const modalId = "iconSelectModal-" + widget.dataset.widgetId;
     const modalElement = document.getElementById(modalId);
 
-    iconButtons.forEach(button => {
-      button.addEventListener("click", function () {
-        const iconClass = this.getAttribute("data-icon-class");
-        const iconName = this.getAttribute("data-icon-name");
+    const iconContainer = modalElement.querySelector(".icon-list");
+    const iconSearch = modalElement.querySelector(".icon-search");
+    const rawJson = iconContainer.dataset.iconsJson;
+    const icons = JSON.parse(rawJson);
 
-        selectedIcon.className = "selected-icon " + iconClass;
-        selectedIconName.textContent = iconName;
-        hiddenInput.value = iconClass;
-        selectedIconContainer.style.display = "block";
+    let iconsLoaded = false;
 
-        // Modal schließen (Bootstrap 5)
-        const modalInstance = bootstrap.Modal.getInstance(modalElement);
-        modalInstance.hide();
+    // Insert Icons during opening modal
+    modalElement.addEventListener("show.bs.modal", function () {
+      if (iconsLoaded) return;
+
+      icons.forEach(icon => {
+        const button = document.createElement("button");
+        button.className = "btn btn-light icon-select-btn";
+        button.setAttribute("type", "button");
+        button.setAttribute("data-icon-name", icon.name);
+        button.setAttribute("data-icon-class", icon.font_class_name);
+        button.innerHTML = `<i class="${icon.font_class_name}"></i>`;
+
+        button.addEventListener("click", function () {
+          selectedIcon.className = "selected-icon " + icon.font_class_name;
+          selectedIconName.textContent = icon.name;
+          hiddenInput.value = icon.font_class_name;
+          selectedIconContainer.style.display = "block";
+
+          bootstrap.Modal.getInstance(modalElement).hide();
+        });
+
+        iconContainer.appendChild(button);
       });
+
+      iconsLoaded = true;
     });
 
+    // Search
     iconSearch.addEventListener("input", function () {
       const filter = this.value.toLowerCase();
-      iconButtons.forEach(button => {
+      iconContainer.querySelectorAll(".icon-select-btn").forEach(button => {
         const name = button.getAttribute("data-icon-name").toLowerCase();
         button.style.display = name.includes(filter) ? "inline-block" : "none";
       });
